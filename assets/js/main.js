d014345d03fbfb7a55839da9e345e4dc92542f5e
Srdaveo28 <76437512+SrDaveo28@users.noreply.github.com>
2021-10-27 17:24:08 -0300

    nueva generacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,3 +112,15 @@ const swiper = new Swiper('.testimonial__container', {
     }
 
 });
+
+/* gsap animation */
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: .5, x: 60})
+
+gsap.from('.home__data', {opacity: 0, duration: 2, delay: .5, y: 25})
+
+gsap.from('.home__greeting, .home__name, home__profession, .home__button', {opacity: 0, duration: 2, delay: 1, y: 25, ease:'expo.out', stagger: .2})
+
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 2, delay: 1.5, y: 25, ease:'expo.out', stagger: .2})
+
+gsap.from('.nav__item', {opacity: 0, duration: 2, delay: 1.8, y: 25, ease:'expo.out', stagger: .2})
+
